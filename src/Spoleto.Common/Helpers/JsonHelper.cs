a440cfaf4908691e37ee17e8f2154e2d5664f7c3
o.nadymov <o.nadymov@spoleto.ru>
2024-03-21 18:31:06 +0300

    ToQueryString

--- src/Spoleto.Common/Helpers/JsonHelper.cs
+++ src/Spoleto.Common/Helpers/JsonHelper.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text.Encodings.Web;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Text.Unicode;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace Spoleto.Common.Helpers
 {
@@ -98,6 +102,45 @@ namespace Spoleto.Common.Helpers
             var body = await JsonSerializer.DeserializeAsync<T>(jsonStream, _defaultSerializerOptions).ConfigureAwait(false);
 
             return body;
+        }
+
+        /// <summary>
+        /// Converts to a query string.
+        /// </summary>
+        public static string ToQueryString<T>(T body)
+        {
+            var bodyJson = ToJson(body);
+            var dictionaryAsObjectValues = FromJson<Dictionary<string, object>>(bodyJson);
+
+            var args = new List<string>();
+            foreach (var key in dictionaryAsObjectValues.Keys)
+            {
+                var jsonValue = (JsonElement)dictionaryAsObjectValues[key];
+                var objValue = FlattenJsonValue(jsonValue);
+                if (objValue is string str)
+                {
+                    args.Add($"{HttpUtility.UrlEncode(key)}={HttpUtility.UrlEncode(str)}");
+                }
+                else if (objValue is IEnumerable enumerable)
+                {
+                    foreach (string item in enumerable)
+                    {
+                        args.Add($"{HttpUtility.UrlEncode(key)}={HttpUtility.UrlEncode(item)}");
+                    }
+                }
+            }
+
+            return string.Join("&", args);
+        }
+
+        private static object FlattenJsonValue(JsonElement objValue)
+        {
+            return objValue.ValueKind switch
+            {
+                JsonValueKind.String => objValue.GetString(),
+                JsonValueKind.Array => objValue.EnumerateArray().Select(FlattenJsonValue),
+                _ => objValue.GetRawText()
+            };
         }
     }
 }
