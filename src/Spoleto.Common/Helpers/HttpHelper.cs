dfd10ad74a52a50daffc24af2810351cf0cee2d4
o.nadymov <o.nadymov@spoleto.ru>
2024-06-05 11:15:14 +0300

    ToStringDictionary

--- src/Spoleto.Common/Helpers/HttpHelper.cs
+++ src/Spoleto.Common/Helpers/HttpHelper.cs
@@ -18,7 +18,7 @@ namespace Spoleto.Common.Helpers
     public static class HttpHelper
     {
         /// <summary>
-        /// Converts the given object to HTTP query string.
+        /// Converts the given object to the HTTP query string.
         /// </summary>
         public static string ToQueryString<T>(T body)
         {
@@ -44,6 +44,39 @@ namespace Spoleto.Common.Helpers
             }
 
             return string.Join("&", args);
+        }
+
+        /// <summary>
+        /// Converts the given object to the string <see cref="Dictionary{String, String}"/>.
+        /// </summary>
+        public static Dictionary<string,string> ToStringDictionary<T>(T body)
+        {
+            var bodyJson = JsonHelper.ToJson(body);
+            var dictionaryAsObjectValues = JsonHelper.FromJson<Dictionary<string, object>>(bodyJson);
+
+            var dictionary = new Dictionary<string, string>();
+            foreach (var key in dictionaryAsObjectValues.Keys)
+            {
+                var jsonValue = (JsonElement)dictionaryAsObjectValues[key];
+                var objValue = FlattenJsonValue(jsonValue);
+                if (objValue is string str)
+                {
+                    dictionary.Add(HttpUtility.UrlEncode(key), HttpUtility.UrlEncode(str));
+                }
+                else if (objValue is IEnumerable enumerable)
+                {
+                    var count = 0;
+                    foreach (string item in enumerable)
+                    {
+                        if (count++ > 0)
+                            throw new ArgumentException("Cannot use IEnumerable argument with more than 1 element to convert to the Dictionary.");
+
+                        dictionary.Add(HttpUtility.UrlEncode(key), HttpUtility.UrlEncode(item));
+                    }
+                }
+            }
+
+            return dictionary;
         }
 
         private static object FlattenJsonValue(JsonElement objValue)
