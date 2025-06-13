05abe1dba1afed2cefcc044f246175adf3f5ad21
o.nadymov <o.nadymov@spoleto.ru>
2025-06-13 15:25:03 +0300

    EnumerableExtension

---
+++ src/Spoleto.Common/Extensions/EnumerableExtension.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Spoleto.Common.Extensions
+{
+    /// <summary>
+    /// Extensions for <see cref="IEnumerable{T}"/>.
+    /// </summary>
+    public static class EnumerableExtension
+    {
+        /// <summary>
+        /// Splits the source into the batches with the given size.
+        /// </summary>
+        public static IEnumerable<IEnumerable<T>> Split<T>(this IEnumerable<T> source, int batchSize)
+        {
+            while (source.Any())
+            {
+                yield return source.Take(batchSize);
+
+                source = source.Skip(batchSize);
+            }
+        }
+    }
+}
